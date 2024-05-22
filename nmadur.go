a091d7443074ce1f18ef9db17ce34d937d3eb9da
fazliddin <hajrullaevfazliddin058@gmail.com>
2024-05-22 18:00:17 +0500

    for practice

--- nmadur.go
+++ nmadur.go
@@ -3,25 +3,5 @@ package main
 import "fmt"
 
 func uniqueMorseRepresentations(words []string) int {
-    slc := []string{".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."}
-    sl := []string{}
-    count := 0
-    for _, i := range words{
-        a := ""
-        for _, j := range i{
-            n := int(j) -97
-            a += slc[n]
-        }
-        for j := 0; j < len(sl); j++{
-            if sl[i] == a{
-                count++
-            }
-        }
-        sl = append(sl, a)
-    }
-    return count
-}
-func main() {
-	a := []string{"adf", "adf", "asd"}
-	fmt.Println(uniqueMorseRepresentations(a))
+    fmt.Println("hello")
 }
